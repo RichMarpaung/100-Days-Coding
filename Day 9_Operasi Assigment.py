4f10b5a704c7a5a08a1bcfdaffebf38c8a25171f
RichMarpaung <115502424+RichMarpaung@users.noreply.github.com>
2022-10-19 20:11:14 +0800

    Add files via upload

---
+++ Day 9_Operasi Assigment.py
@@ -0,0 +1,27 @@
+''' Operasi Assigment'''
+a = 5
+
+print("Nilai a : ",a)
+a += 1 #Artinya a = a + 1
+print("Nilai a+= 1 , menjadi bernilai : ",a)
+a -= 2 # artinya a = a - 2
+print("Nilai a-= 2 , menjadi bernilai : ",a)
+a *= 3 # artinya a = a * 3
+print("Nilai a*= 3 , menjadi bernilai : ",a)
+a /= 2 # artinya a = a / 2
+print("Nilai a/= 2 , menjadi bernilai : ",a)
+
+b = 16
+print("\nNilai b : ",b)
+b //= 2 # artinya b = b // 2
+print("Nilai b//= 2 , menjadi bernilai : ",b)
+
+c = 16
+print("\nNilai c : ",c)
+c %= 2 # artinya c = c % 2
+print("Nilai c//= 2 , menjadi bernilai : ",c)
+
+d = 5
+print("\nNilai d : ",d)
+d **= 2 # artinya d = d** 2
+print("Nilai **= 2 , menjadi bernilai : ",d)
