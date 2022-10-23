55556a6fb0f1073dd7b76bcdc308fc5766fcff18
RichMarpaung <115502424+RichMarpaung@users.noreply.github.com>
2022-10-23 21:35:47 +0800

    Add files via upload

---
+++ Day 13_Percabangan.py
@@ -0,0 +1,14 @@
+'''Percabangan'''
+
+data = input("Masukkan angka 1-4 : ")
+
+if data == '1':
+    print("Satu")
+elif data == '2':
+    print("Dua")
+elif data == '3':
+    print("Tiga")
+elif data == '4':
+    print("Empat")
+else:
+    print("Angka yang anda masukkan tidak sesuai")
