0a1e417e468fe3606fd48e9ae9f1d2fa32162bd3
RichMarpaung <115502424+RichMarpaung@users.noreply.github.com>
2022-12-20 11:49:30 +0800

    Add files via upload

---
+++ Day 71_Pengenalan.py
@@ -0,0 +1,8 @@
+data_bool = True
+data_string = "rich"
+data_integer = 23
+data_float = 2.5
+print(data_bool,type(data_bool))
+print(data_string,type(data_string))
+print(data_integer)
+print(data_float)
