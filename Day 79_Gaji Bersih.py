c42ba574f9a774fa83e77a63f9efc5c3aceb4b09
RichMarpaung <115502424+RichMarpaung@users.noreply.github.com>
2022-12-28 21:11:09 +0800

    Add files via upload

---
+++ Day 79_Gaji Bersih.py
@@ -0,0 +1,6 @@
+gaji = int(input("Masukkan Gaji Pokok : "))
+lembur = int(input("Masukkan Jam Lembur : "))
+gaji_lembur = 55000
+
+gaji_bersih = gaji+(lembur*gaji_lembur)
+print(f"Gaji Bersih Anda Adalah : {gaji_bersih}")
